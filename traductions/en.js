ccdf4196844bb9f3273b9a375f151c087f7f7694
A Clever <aclever@mail.be>
2024-02-15 11:24:58 +0100

    add traduction

---
+++ traductions/en.js
@@ -0,0 +1,8 @@
+export const BUTTONS_CALENDAR = {
+    backToToday: "Back to today",
+    backToSelectedDate: "Back to the selected date",
+    nextMonth: "Next month",
+    nextYear: "Next year",
+    prevMonth: "Previous month",
+    prevYear: "Previous year",
+}
---
+++ traductions/fr.js
@@ -0,0 +1,8 @@
+export const BUTTONS_CALENDAR = {
+    backToToday: "Revenir à aujourd'hui",
+    backToSelectedDate: "Revenir à la date sélectionnée",
+    nextMonth: "Mois suivant",
+    nextYear: "Année suivante",
+    prevMonth: "Mois précédent",
+    prevYear: "Année précédente",
+}
